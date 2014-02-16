3dba8c3df375e63df1db39f8c88ddd6545416226
최욱주 <starmiso@naver.com>
2014-02-16 22:56:38 +0900

    커리큘럼 테이블 문제 줄바꿈 문제 수정
    - CSS 수정함.

--- bdnews/select_curriculum.js
+++
@@ -1,9 +0,0 @@
-function SelectCurriculum(){
-
-		var selected = document.getElementById("groupList").selectedIndex;
-		var filename = document.getElementsByTagName("option")[selected].value;
-
-		var URL =  filename + '.html';
-
-		window.location = URL;
-	}
--- javascripts/select_curriculum.js
+++ javascripts/select_curriculum.js
@@ -1,3 +1,4 @@
+
 function SelectCurriculum(){
 
 		var selected = document.getElementById("groupList").selectedIndex;
@@ -7,3 +8,19 @@ function SelectCurriculum(){
 
 		window.location = URL;
 	}
+
+
+
+function ShowMoreContents(idMyDiv, idLabel){
+     var objDiv = document.getElementById(idMyDiv);
+     var label =  document.getElementById(idLabel);
+
+     if(objDiv.style.display=="block"){ 
+     	objDiv.style.display = "none"; 
+     	label.value = "결과물 더 보기";
+ 		}
+     else{ 
+     	objDiv.style.display = "block"; 
+     	label.value = "결과물 감추기";
+     }
+}
